b6fb131295ec5bc343c71cf11c5045d820e6cf14
Lasu-dev <appsu.1711@gmail.com>
2024-05-12 11:27:33 -0300

    subiendo las clases que faltan

---
+++ CLASE-11/js.md
@@ -0,0 +1,5 @@
+### Que es JavaScrip?
+
+Es un lengiaje de programación interpretado por v8engine.
+Se ejecuta en el navegador
+Es un lenguaje de alto nivel
---
+++ CLASE-11/script.js
@@ -0,0 +1,60 @@
+/* TIPOS DE DATOS */ /* ALT + SHIFT + A */
+
+/* Datos obejo: proximamente..... */
+
+/* Datos primitivos
+
+Number= Ej:
+8
+0
+-2
+3.14 (no es decimal, es flotante (float)) 
+Nan
+Infinity
+
+String => textos, deben ir entre comillas Ej:
+'hola mundo'
+"Pepe"
+'8'
+NaN
+
+Boolean (George Boole) => valores true-false
+
+Null => ausencia o nulidad de un dato
+ej:
+null
+
+Undefined => no hay un dato definido
+ej:
+Undefined
+*/
+
+/*Soy un mensajero que envía una carta, llega a la casa , me atienden y me dicen que NO quieren recibir la carta
+estado_de_respuesta: false 
+
+Soy un mensajero que envía una carta, llega a la casa , me atienden y me dicen que  quieren recibir la carta
+estado_de_respuesta: true
+
+Soy un mensajero que envía una carta, me extravío en el camino
+estado_de_respuesta: undefined
+
+Soy un mensajero que envía una carta, llega a la casa , no me atienden
+estado_de_respuesta: null*/
+
+
+
+/* Operadores aritméticos (typeof nos dice ante que tipo de dato estamos ej. typeof (1 + 1) = number) */
+
+/*SIEMPRE DEVUELVE STRING:
+
++ = CONCATENACIÓN
+
+/*SIEMPRE DEVUELVE NUMEROS:
+
++ = Suma
+- = resta
+* = multiplicación
+/ = división
+% = resto de la división o módulo
+** = potencia
+*/
